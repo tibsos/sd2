e3d5701c3b5b228e3d264ecc40e9dca8f6b37119
tibsos <kosyakovsn@gmail.com>
2024-05-26 05:12:05 +0300

    x

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+from uuid import uuid4
+
+class Customer(m.Model):
+
+    uid = m.UUIDField(default = uuid4)
+
+    name = m.TextField()
+    phone = m.TextField()
+
+    sent_at = m.DateTimeField(auto_now_add = True)
+    updated_at = m.DateTimeField(auto_now = True)
+
+    def __str__(self):
+
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+from .models import *
 
 def landing(request):
 
     return render(request, 'landing.html')
+
+def receive_contact_info(request):
+
+    name = request.POST.get('n')
+    phone = request.POST.get('p')
+
+    Customer.objects.create(name = name, phone = phone)
+
+    return HttpResponse('K')
